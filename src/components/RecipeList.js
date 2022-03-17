38fa45d552b7eac5698a2ea08493473b8a83f6c5
Matt Grogan <mattgrogan.dev@gmail.com>
2022-03-17 01:29:12 -0700

    changed over to recipe app and built out left portion w recipe list

---
+++ src/components/RecipeList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+const RecipeList = ({recipes}) => {
+  return (
+    <>
+    <div>
+        {recipes.map(recipe => {
+        return ( 
+        <Recipe 
+        key={recipe.id} 
+        {...recipe} />
+        )
+        })}
+    </div>
+    <button>Add Recipe</button>
+    </>
+
+    
+  )
+}
+
+export default RecipeList
