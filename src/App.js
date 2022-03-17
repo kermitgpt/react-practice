2669660ed55da94053008bc1453868e6e29b6d19
Matt Grogan <mattgrogan.dev@gmail.com>
2022-03-16 22:10:57 -0700

    added themecontext to change button colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import Counter from './components/Counter'
 
+export const ThemeContext = React.createContext()
+
 function App() {
+
+  const [theme, setTheme] = useState('red')
+
   return (
-    <Counter />
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+    <Counter initialCount={0} />
+    <button onClick={() => setTheme(prevTheme => {
+      return prevTheme === 'red' ? 'blue' : 'red'
+    })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
-import {useState} from 'react';
-const Counter = () => {
-  console.log('Render Counter Hooks')
-  const [count, setCount] = useState(0)
+import {useState, useContext} from 'react';
+import { ThemeContext } from '../App';
+
+const Counter = ({initialCount}) => {
+ 
+  const [count, setCount] = useState(initialCount)
+  const style = useContext(ThemeContext)
   return (
     <div>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button style={style} onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
       <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button style={style} onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
     </div>
   );
 };
