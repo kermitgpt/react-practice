5299ceed2089bd1610e654270a4008838d222b7b
Matt Grogan <mattgrogan.dev@gmail.com>
2022-03-16 22:13:49 -0700

    deleted useeffect to try to fix netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Counter from './components/Counter'
 
 export const ThemeContext = React.createContext()
